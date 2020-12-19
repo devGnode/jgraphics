6f27a2f451b287095682c7dcd3cb5cf27bfa5abc
maroder <no-reply@provider.tld>
2020-12-18 15:23:46 -1100

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+import GText from "./lib/GText.js";
+import GraphicsElement from "./lib/GraphicsElement.js";
+import Layout from "./lib/Layout.js";
+import FlowLayout from "./lib/FlowLayout.js";
+import BoxLayout from "./lib/BoxLayout.js";
+
+window.BoxLayout = BoxLayout;
+window.FlowLayout = FlowLayout;
+window.Layout = Layout;
+window.GraphicsElement = GraphicsElement;
+window.GText = GText;
